789dd68d00ea5b16e51803c86823d7be69704a77
Sayani Koley <sayanikoley8@gmail.com>
2021-06-10 11:36:03 +0530

    Welcome message to initialise the start position of the game as 0

---
+++ SnakeAndLadderProblem.java
@@ -0,0 +1,5 @@
+public class SnakeAndLadderProblem {
+    public static void main(String[] args) {
+        System.out.println("Snake and Ladder game played with single player at start position 0");
+    }
+}
