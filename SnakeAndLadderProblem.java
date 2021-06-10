2d100f34078e6e2065b3c9714db5cbb87c921520
Sayani Koley <38989758+sayani2014@users.noreply.github.com>
2021-06-10 17:07:51 +0530

    Initialize the game

--- SnakeAndLadderProblem.java
+++ SnakeAndLadderProblem.java
@@ -1,3 +1,8 @@
+/* Initialise the game
+ *
+ * @author: SAYANI KOLEY
+ * DATE: 10/06/2021
+ */
 public class SnakeAndLadderProblem {
     public static void main(String[] args) {
         System.out.println("Snake and Ladder game played with single player at start position 0");
