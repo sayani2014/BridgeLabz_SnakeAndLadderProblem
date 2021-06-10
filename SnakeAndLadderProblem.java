69cead19adddc1a3b0185cfd0f03d946a16a2cb2
Sayani Koley <38989758+sayani2014@users.noreply.github.com>
2021-06-10 17:09:30 +0530

    Update SnakeAndLadderProblem.java

--- SnakeAndLadderProblem.java
+++ SnakeAndLadderProblem.java
@@ -1,3 +1,9 @@
+/* Roll the dice using Random()
+ *
+ * @author: SAYANI KOLEY
+ * DATE: 10/06/2021
+ */
+
 public class SnakeAndLadderProblem {
     public static void main(String[] args) {
         int roll_die = (int)((Math.floor(Math.random() * 10)) % 6) + 1;
