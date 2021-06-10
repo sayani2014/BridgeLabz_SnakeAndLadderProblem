266fd2b46dad67319bf7cbab25f7e14fbab14258
Sayani Koley <sayanikoley8@gmail.com>
2021-06-10 12:50:12 +0530

    Added repeat till the player reaches the winning position 100

--- SnakeAndLadderProblem.java
+++ SnakeAndLadderProblem.java
@@ -5,27 +5,29 @@ public class SnakeAndLadderProblem {
 
     public static void main(String[] args) {
         int initial_position = 0;
-        int roll_die = (int)((Math.floor(Math.random() * 10)) % 6) + 1;
-        int check_option = (int)((Math.floor(Math.random() * 10)) % 3);
-        switch (check_option) {
-            case noPlay:
+        int final_position = 100;
+        while (initial_position <= final_position) {
+            int roll_die = (int) ((Math.floor(Math.random() * 10)) % 6) + 1;
+            int check_option = (int) ((Math.floor(Math.random() * 10)) % 3);
+            switch (check_option) {
+                case noPlay:
+                    //initial_position = 0;
+                    System.out.println("Its a NO PLAY! Player stays in the same position!");
+                    break;
+                case ladder:
+                    initial_position += roll_die;
+                    System.out.println("Its a LADDER! Player moves ahead by " + roll_die + " positions.");
+                    break;
+                case snake:
+                    initial_position -= roll_die;
+                    System.out.println("Its a SNAKE! Player moves behind by " + roll_die + " positions.");
+                    break;
+            }
+            System.out.println("Position reached by the player: " +initial_position);
+            if (initial_position < 0)
                 initial_position = 0;
-                System.out.println("Its a NO PLAY! Player stays in the same position!");
-                break;
-            case ladder:
-                initial_position += roll_die;
-                System.out.println("Its a LADDER! Player moves ahead by " +roll_die+ " positions.");
-                break;
-            case snake:
-                initial_position -= roll_die;
-                System.out.println("Its a SNAKE! Player moves behind by " +roll_die+ " positions.");
-                break;
-            default:
-                System.out.println("Error! Please check!");
-                break;
         }
-        if(initial_position < 0)
-            initial_position = 0;
-        System.out.println("Current position of the player in the game is: " +initial_position);
+	System.out.println();
+        System.out.println("Player completes the game! Reached the winning position 100.");
     }
 }
